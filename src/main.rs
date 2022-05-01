1a370322ad9bb3617e15da344d071902d8e9541f
EdenQwQ <lsahlm1eden@gmail.com>
2022-05-01 15:16:40 +0800

    [update] added other options

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
 use clap::{arg, Command};
 use fsio::file;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::Path;
 
 fn main() {
     let args = clap_args().get_matches();
 
     let (conn, _) = xcb::Connection::connect(None).unwrap();
-    if args.is_present("in") {
-        let focused = xcb::get_input_focus(&conn).get_reply().unwrap().focus();
-        push(focused);
-        xcb::unmap_window_checked(&conn, focused)
+    if args.is_present("push") {
+        let option = args.value_of("push").unwrap();
+        let window = match option {
+            "focused" => xcb::get_input_focus(&conn).get_reply().unwrap().focus(),
+            "selected" => {
+                let selected = std::process::Command::new("sh")
+                    .arg("-c")
+                    .arg("xdotool selectwindow")
+                    .output()
+                    .unwrap()
+                    .stdout;
+                String::from_utf8(selected)
+                    .unwrap()
+                    .trim()
+                    .parse::<u32>()
+                    .unwrap()
+            }
+            _ => option.parse::<u32>().unwrap(),
+        };
+        push(window);
+        xcb::unmap_window_checked(&conn, window)
             .request_check()
             .unwrap();
         conn.flush();
-    } else if args.is_present("out") {
-        let out = pop();
-        xcb::map_window_checked(&conn, out).request_check().unwrap();
+    } else if args.is_present("pop") {
+        let option = args.value_of("pop").unwrap();
+        let window = match option {
+            "last" => file::read_text_file("/tmp/srsp.tmp")
+                .unwrap()
+                .lines()
+                .last()
+                .unwrap()
+                .parse::<u32>()
+                .unwrap(),
+            _ => option.parse::<u32>().unwrap(),
+        };
+        pop(&window);
+        xcb::map_window_checked(&conn, window)
+            .request_check()
+            .unwrap();
         conn.flush();
     }
 }
@@ -28,16 +55,16 @@ pub fn clap_args() -> Command<'static> {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            arg!(-i --in)
+            arg!(-i - -push)
                 .required(false)
-                .takes_value(false)
+                .takes_value(true)
                 .help("Push the focused window into the scratchpad"),
         )
         .arg(
-            arg!(-o - -out)
+            arg!(-o - -pop)
                 .required(false)
-                .takes_value(false)
-                .help("Pop out the last window from the scratchpad"),
+                .takes_value(true)
+                .help("Pop out a window from the scratchpad"),
         );
     app
 }
@@ -47,14 +74,13 @@ pub fn push(window: u32) {
     file::append_text_file("/tmp/srsp.tmp", &format!("{}\n", window)).unwrap();
 }
 
-pub fn pop() -> u32 {
-    let content = file::read_text_file("/tmp/srsp.tmp").unwrap();
-    let last_window = content.lines().last().unwrap();
-    let new_content = content
-        .lines()
-        .take(content.lines().count() - 1)
-        .map(|i| format!("{}\n", i))
-        .collect::<String>();
-    file::write_text_file("/tmp/srsp.tmp", &new_content).unwrap();
-    last_window.parse::<u32>().unwrap()
+pub fn pop(window: &u32) {
+    let mut new = String::new();
+    for line in file::read_text_file("/tmp/srsp.tmp").unwrap().lines() {
+        if line.parse::<u32>().unwrap() == *window {
+            continue;
+        }
+        new.push_str(&format!("{}\n", line));
+    }
+    file::write_text_file("/tmp/srsp.tmp", &new).unwrap();
 }
